1f61e4f077cbb31709bd3d246c8e62a3932d8b68
ihaneul <789_skymert@naver.com>
2021-03-12 13:01:55 +0900

    error

---
+++ app/src/main/java/com/endcorona/coketlist/ui/LogInActivity.kt
@@ -0,0 +1,18 @@
+package com.endcorona.coketlist.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.endcorona.coketlist.R
+import com.endcorona.coketlist.databinding.ActivityLogInBinding
+
+class LogInActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityLogInBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_log_in)
+
+
+    }
+}
