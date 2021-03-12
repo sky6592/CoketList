1f62cb8e7d837fda80a1d3cced40e5310d1c70e8
ihaneul <789_skymert@naver.com>
2021-03-12 19:05:16 +0900

    add : fragment(mycoket, space)

--- app/src/main/java/com/endcorona/coketlist/ui/space/SpaceFragment.kt
+++ app/src/main/java/com/endcorona/coketlist/ui/space/SpaceFragment.kt
@@ -1,6 +1,18 @@
 package com.endcorona.coketlist.ui.space
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class SpaceFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
