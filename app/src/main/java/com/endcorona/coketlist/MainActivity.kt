cd120c99e87b3b2f9ecdf3ed2c88e2776c7e470e
ihaneul <789_skymert@naver.com>
2021-03-12 19:45:58 +0900

    add : main - bottomNavigation

--- app/src/main/java/com/endcorona/coketlist/MainActivity.kt
+++ app/src/main/java/com/endcorona/coketlist/MainActivity.kt
@@ -2,10 +2,30 @@ package com.endcorona.coketlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.endcorona.coketlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //기존 등록한 Theme에 의해 구조적으로 포함되어이쓴 Timt를 초기화 시켜준다
+        mBinding.mainNavi.itemIconTintList = null
+
+
+
+        mBinding.mainNavi.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.menu_my_coket ->{
+
+                }
+                R.id.menu_space -> {
+
+                }
+            }
+        }
+
     }
 }
